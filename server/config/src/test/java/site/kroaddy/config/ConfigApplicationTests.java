4f3a1adee3405daf6b559d64acbebea2113d2a98
kangsungunn <kangsungunn@gmail.com>
2025-11-12 17:52:01 +0900

    251112 뼈대(유레카만 띄우기)

---
+++ server/config/src/test/java/site/kroaddy/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package site.kroaddy.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ service/restaurant/src/main/java/site/kroaddy/restaurant/RestaurantApplication.java
@@ -0,0 +1,17 @@
+package site.kroaddy.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class RestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantApplication.class, args);
+	}
+
+}
